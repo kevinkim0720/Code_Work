7c196303d3d13d01160694e10ae285d3f15ea71a
Kevin Kim <89947380+kevinkim0720@users.noreply.github.com>
2021-09-01 19:11:55 -0400

    Add files via upload

---
+++ Build a Bunny in a Box.py
@@ -0,0 +1,13 @@
+#Client name: Dr. Amy Connolly
+
+#Programmer name: Kevin Kim
+
+#PA purpose: Drawing a bunny in a box
+
+#My submission of this program indicates that I have neither received nor given substantial or unauthorized assistance in writing this program.
+
+print("----------------")
+print("| (\ (\        |")
+print("|  (-.-)       |")
+print("|o_ ('') ('')  |")
+print("XXXXXXXXXXXXXXXX")
